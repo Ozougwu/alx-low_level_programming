205737c3adae98dac873081c2d644f4a117f6765
Ozougwu <ozougwucchristian@gmail.com>
2022-03-13 23:32:49 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of
+ * Description: prints "with proper grammar, but the outcome is a piece of 
  * art,"
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, /n);
+	printf("with proper grammar, but the outcome is a piece of art,/n");
 	return (0);
 }
+
