0ee2f9dec2b1aaa716e5ee2949f0018b73aba58a
Ozougwu <ozougwucchristian@gmail.com>
2022-03-16 03:16:30 -0700

    n

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *  _abs - computes the absolute value for an interger
- *  @n: interger to be tested
- *  Return: 0 or 1
+ * _abs - computes the absolute value for an interger
+ * @n: interger to be tested
+ * Return: 0 or 1
  */
 
 int _abs(int n)
