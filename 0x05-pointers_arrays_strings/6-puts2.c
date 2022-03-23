802228f06dffd81ce849c0bdad871f67f2096a6a
Ozougwu <ozougwucchristian@gmail.com>
2022-03-22 18:03:10 -0700

    p

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
- * puts2 - prints a string, followed by a new line,
- * @str: pointer to the string to print
+ * puts2 - print alternating chars of string
+ * @str: string
  * Return: void
  */
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
-		{
 			_putchar(str[i]);
-		}
-
 		i++;
 	}
 	_putchar('\n');
